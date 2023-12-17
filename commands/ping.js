d47dc89ef95d3d81d2de0b64c69055e396c524e8
AnimateObject <110643766+JDNorman@users.noreply.github.com>
2023-12-17 15:59:56 -0600

    Update ping.js
    Formatting

--- commands/ping.js
+++ commands/ping.js
@@ -1,41 +1,40 @@
 //Any other imports go here
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 const color = 100000;
 
 module.exports = {
-  
   //Write command build in here
-  data: pingSLASH = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('The most complicated ping embed command ever')
-  .addStringOption(option =>
-    option.setName('text')
-                   .setDescription('The text to echo back')
-                   .setRequired(false))
-  .addUserOption(option =>
-    option.setName('target')
-                 .setDescription('The user to echo back')
-                 .setRequired(false)), //put the comma at the end of the line to signify the end of the command build and the start of the command execute
+  data: (pingSLASH = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("The most complicated ping embed command ever")
+    .addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("The text to echo back")
+        .setRequired(false),
+    )
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("The user to echo back")
+        .setRequired(false),
+    )), //put the comma at the end of the line to signify the end of the command build and the start of the command execute
 
   //Write command execute in here
-  async execute(int) /*int short for interaction*/ 
-  {
-    
+  async execute(int /*int short for interaction*/) {
     console.log(`A ping command was used!`);
-    
+
     //pull information from the command builder
-    const user = int.options.getUser('target');
-    const msg = int.options.getString('text');
+    const user = int.options.getUser("target");
+    const msg = int.options.getString("text");
     const channel = int.channel;
     const username = user.username;
     const member = int.member;
     const membername = member.user.username;
     const ephemeralMSG = "Pong!";
-    const visibleMSG = `${username}, ${membername} wanted to let you know that ${msg}!`
-    
+    const visibleMSG = `${username}, ${membername} wanted to let you know that ${msg}!`;
+
     int.reply({ epemeral: true, content: ephemeralMSG });
     channel.send({ epemeral: false, content: visibleMSG });
-    
   },
-    
 };
